040e5dd7e416f5b23beb738667e75558e8cab5d3
ambergon <foxgon@outlook.jp>
2021-01-10 23:47:23 +0900

    [change]
    summaryの行を不要にし、detailからタイトル、クラスを設定できるように変更
    またsummaryによってpタグが崩れる問題、summaryの次の行を空行にしなければエラーが出る問題を解消

--- a_markdown_details.py
+++ a_markdown_details.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 """
 Markdown Custom class extension for Python-Markdown
 =========================================
@@ -5,17 +6,10 @@ Markdown Custom class extension for Python-Markdown
     >>> import markdown
     >>> md = markdown.Markdown(extensions=['a_markdown_details'])
 
-    >>> md.convert('{{##summary_title}}aaa')
+
+    >>> md.convert('{{{##title')
+    >>> md.convert('}}}')
     u'<p><summary class="h2 aaa">summary_title</summary></p>'
-
-    >>> md.convert('{{##summary_title}}')
-    u'<p><summary class="h2 ">summary_title</summary></p>'
-
-    >>> md.convert('{{summary_title}}aaa')
-    u'<p><summary class="aaa">summary_title</summary></p>'
-
-    >>> md.convert('{{summary_title}}')
-    u'<p><summary class="">summary_title</summary></p>'
 
 """
 from __future__ import absolute_import
@@ -30,45 +24,51 @@ import xml.etree.ElementTree as etree
 #--div--
 
 #detail
-RE_FENCE_START = r'^[{]{3}$'
-RE_FENCE_END = r'^[}]{3}$'
-# RE_FENCE_START = r'^{{3}\n'
-# RE_FENCE_END = r'}{3}'
+# RE_FENCE_START = r'^[{]{3}$'
+# RE_FENCE_END = r'^[}]{3}$'
+
+RE_FENCE_START = r'^{{3}(?P<class>#*)(?P<title>.*)'
+#RE_FENCE_START = r'^{{3}\n'
+RE_FENCE_END = r'}{3}'
 
 
 
-#{{summary}} >> <summary>summary_title</summary>
-SUMMARY = r'^[{]{2}(?P<class>#*)(?P<title>.+?)[}]{2}(?P<class_option>.*)$'
-#SUMMARY = r'^[{]{2}(?P<title>.+?)[}]{2}(?P<class>.*)$'
+#SUMMARY = r'[{]{2}(?P<class>#*)(?P<title>.+?)[}]{2}(?P<class_option>.*)'
+SUMMARY = r'[{]{2}(?P<class>#*)(?P<title>.+?)[}]{2}(?P<class_option>.*)$'
 
 class MyExtension(Extension):
     def extendMarkdown(self, md, md_globals):
 
-        #summary
-        summary=SummaryPattern(SUMMARY, md)
-        md.inlinePatterns['custom_summary'] = summary
-    
-        #detail
+        #detail&summary
         md.parser.blockprocessors.register(DetailBlock(md.parser),'box',175)
-
 
 class SummaryPattern(Pattern):
     def handleMatch(self, matched):
 
-        text = matched.group("title")
+        title = matched.group("title")
         cls = matched.group("class")
         cls_option = matched.group("class_option")
-        line = markdown.util.etree.Element("summary")
 
+        cls_str=''
         if cls != "" :
             count = len(cls)
-            cls_str="h" + str(count) + " "+cls_option
-        else :
-            cls_str=cls_option
-        
-        line.set("class", cls_str )
-        line.text = markdown.util.AtomicString(text)
+            cls_str=cls_str + 'h'+str(count)
+
+        if cls_option != '':
+            cls_str=cls_str + self.check_str(cls_str) + cls_option
+
+        # line = markdown.util.etree.Element("p")
+        # summary = markdown.util.etree.SubElement(line , "summary")
+        # summary.set("class", cls_str )
+        # summary.text = markdown.util.AtomicString(title)
+
         return line
+    def check_str(self , text ):
+        if text == '':
+            return ''
+        else :
+            return ' '
+
 
 class DetailBlock(BlockProcessor):
     def test(self,parent,block):
@@ -76,12 +76,27 @@ class DetailBlock(BlockProcessor):
 
     def run(self,parent,blocks):
         original_block = blocks[0]
+
+        pattern = re.compile(RE_FENCE_START)
+        match = pattern.search(blocks[0])
+        title = match.group("title")
+        cls = match.group("class")
+        cls_str=''
+        if cls != "" :
+            count = len(cls)
+            cls_str=cls_str + 'h'+str(count)
+
         blocks[0] = re.sub(RE_FENCE_START,'',blocks[0])
 
         for block_num,block in enumerate(blocks):
             if re.search(RE_FENCE_END,block):
                 blocks[block_num] = re.sub(RE_FENCE_END,'',block)
                 e = etree.SubElement(parent,'details')
+
+                summary = markdown.util.etree.SubElement( e , "summary")
+                summary.set("class", cls_str )
+                summary.text = markdown.util.AtomicString(title)
+
                 #e.set('open','')
                 #e.set('style','display: inline-block; border: 1px solid red;')
                 self.parser.parseBlocks(e,blocks[0:block_num + 1])
