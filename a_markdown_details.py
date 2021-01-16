da8faf3e84d3a67fdc6ca2c810672528af80e972
ambergon <foxgon@outlook.jp>
2021-01-17 00:37:05 +0900

    [change]
    unittestに対応
    versionを0.0.3に

--- a_markdown_details.py
+++ a_markdown_details.py
@@ -1,17 +1,4 @@
 # coding:utf-8
-"""
-Markdown Custom class extension for Python-Markdown
-=========================================
-
-    >>> import markdown
-    >>> md = markdown.Markdown(extensions=['a_markdown_details'])
-
-
-    >>> md.convert('{{{##title')
-    >>> md.convert('}}}')
-    u'<p><summary class="h2 aaa">summary_title</summary></p>'
-
-"""
 from __future__ import absolute_import
 from __future__ import unicode_literals
 import markdown
@@ -21,53 +8,16 @@ from markdown.blockprocessors import BlockProcessor
 import re
 import xml.etree.ElementTree as etree
 
-#--div--
-
-#detail
-# RE_FENCE_START = r'^[{]{3}$'
-# RE_FENCE_END = r'^[}]{3}$'
+import unittest
 
 RE_FENCE_START = r'^{{3}(?P<class>#*)(?P<title>.*)'
-#RE_FENCE_START = r'^{{3}\n'
 RE_FENCE_END = r'}{3}'
-
-
-
-#SUMMARY = r'[{]{2}(?P<class>#*)(?P<title>.+?)[}]{2}(?P<class_option>.*)'
-SUMMARY = r'[{]{2}(?P<class>#*)(?P<title>.+?)[}]{2}(?P<class_option>.*)$'
 
 class MyExtension(Extension):
     def extendMarkdown(self, md, md_globals):
 
         #detail&summary
         md.parser.blockprocessors.register(DetailBlock(md.parser),'box',175)
-
-class SummaryPattern(Pattern):
-    def handleMatch(self, matched):
-
-        title = matched.group("title")
-        cls = matched.group("class")
-        cls_option = matched.group("class_option")
-
-        cls_str=''
-        if cls != "" :
-            count = len(cls)
-            cls_str=cls_str + 'h'+str(count)
-
-        if cls_option != '':
-            cls_str=cls_str + self.check_str(cls_str) + cls_option
-
-        # line = markdown.util.etree.Element("p")
-        # summary = markdown.util.etree.SubElement(line , "summary")
-        # summary.set("class", cls_str )
-        # summary.text = markdown.util.AtomicString(title)
-
-        return line
-    def check_str(self , text ):
-        if text == '':
-            return ''
-        else :
-            return ' '
 
 
 class DetailBlock(BlockProcessor):
@@ -109,9 +59,18 @@ class DetailBlock(BlockProcessor):
 def makeExtension(*args, **kwargs):
     return MyExtension(*args, **kwargs)
 
+class TextClass(unittest.TestCase):
+    def test_extends(self):
+        import markdown
+        md = markdown.Markdown(extensions=['a_markdown_details'])
+
+       #日本語を単独で使えるようにしたい 
+        text = '''{{{##title
+hoge
+hoge1
+}}}'''
+        print md.convert(text)
+
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod(
-            #verbose=True
-            )
+    unittest.main()
     
