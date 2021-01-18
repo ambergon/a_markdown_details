ec539b2d0e55afa4e07f1db1c7c6c2cbc793e950
ambergon <foxgon@outlook.jp>
2021-01-19 00:34:09 +0900

    [fix]

--- a_markdown_details.py
+++ a_markdown_details.py
@@ -1,6 +1,4 @@
 # coding:utf-8
-from __future__ import absolute_import
-from __future__ import unicode_literals
 import markdown
 from markdown import Extension
 from markdown.inlinepatterns import Pattern
